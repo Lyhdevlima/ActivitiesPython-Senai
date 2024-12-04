8188000018d1320867342124b10ef1fa752fcb90
Lyhdevlima <lima.wendel2018@gmail.com>
2024-12-04 11:14:28 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,5 @@
+print('Hello, World!')
+print('Today is the fourth of december, and i am hungry')
+print('I am in search of a new thing to do, next year i will start college')
+print('i do not know what to write here, i wanna start an ADS degree at FATEC, but i am not so sure if i will be able to stay at my current job')
+print('My Spotify retrospective looks way different than what i thought it would look like')
